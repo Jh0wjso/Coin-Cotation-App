9c164579734834828b2f1c61491e175c11822d35
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-10-11 23:44:55 -0300

    Header UserScreen

--- src/components/UserScreen/index.js
+++ src/components/UserScreen/index.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { View, StatusBar } from "react-native";
+import { View, StatusBar, Text } from "react-native";
 import styles from "./styles";
 
 export default function UserScreen() {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#B22222" barStyle={"light-content"} />
+      <View style={styles.header}>
+        <View style={styles.headerContent}>
+          <View style={styles.logoName} />
+          <Text style={styles.title}>Opçoes</Text>
+          <Text style={styles.secondName}>De Usuario</Text>
+        </View>
+      </View>
     </View>
   );
 }
--- src/components/UserScreen/styles.js
+++ src/components/UserScreen/styles.js
@@ -5,6 +5,35 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#B22222",
   },
+  header: {
+    backgroundColor: "#ffffff",
+    width: "100%",
+    borderBottomLeftRadius: 50,
+  },
+  headerContent: {
+    backgroundColor: "#ffffff",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    borderBottomLeftRadius: 100,
+    paddingTop: 20,
+  },
+  logoName: {
+    flexDirection: "column",
+  },
+  secondName: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "#f29407",
+    fontStyle: "italic",
+    paddingLeft: 110,
+    paddingBottom: 9,
+  },
+  title: {
+    color: "#B22222",
+    fontSize: 30,
+    fontWeight: "bold",
+  }
 });
 
 export default styles;
