7f4c69a0efc759ef4d341044ff77eace021be91a
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-10-31 20:21:40 -0300

    Removing Login and register screens

--- src/components/routes.js
+++ src/components/routes.js
@@ -5,25 +5,21 @@ import GenericCoinScreen from "./GenericCoinScreen";
 import HomeScreenStack from "./HomeScreenStack";
 import InfoScreen from "./InfoScreen";
 import MarketCoinScreen from "./MarketCoinScreen";
-import LoginScreen from "./LoginScreen";
-import RegisterScreen from "./RegisterScreen";
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
   return (
     <Stack.Navigator
-      initialRouteName="LoginScreen"
+      initialRouteName="HomeScreenStack"
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="LoginScreen" component={LoginScreen} />
       <Stack.Screen name="HomeScreenStack" component={HomeScreenStack} />
       <Stack.Screen name="GenericCoinScreen" component={GenericCoinScreen} />
       <Stack.Screen name="MarketCoinScreen" component={MarketCoinScreen} />
       <Stack.Screen name="InfoScreen" component={InfoScreen} />
-      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
     </Stack.Navigator>
   );
 }
