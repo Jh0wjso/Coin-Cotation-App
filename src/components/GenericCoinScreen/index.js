a4ae1c10f2bec6205d50008b33609abc15e69c2b
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-11-07 10:56:04 -0300

    Update on project

--- src/components/GenericCoinScreen/index.js
+++ src/components/GenericCoinScreen/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, StatusBar } from "react-native";
+import { View, Text, TouchableOpacity, StatusBar, Modal } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import styles from "./styles";
 
@@ -32,15 +32,18 @@ export default function GenericCoinScreen({ navigation, route }) {
     coinInfos();
   });
 
+  const [modalVisible, setModalVisible] = useState(false);
+  const [lastSaleModalVisible, setLastSaleModalVisible] = useState(false);
+  const [openValueModalVisible, setOpenValueModalVisible] = useState(false);
+  const [volModalVisible, setVolModalVisible] = useState(false);
+
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#FFFFFF" barStyle={"dark-content"} />
       <View style={styles.headerBackground}>
         <View style={styles.header}>
           <View style={styles.headerContent}>
-            <TouchableOpacity
-              onPress={() => navigation.goBack()}
-            >
+            <TouchableOpacity onPress={() => navigation.goBack()}>
               <Ionicons
                 name={"arrow-back-outline"}
                 style={styles.closeIcon}
@@ -62,39 +65,161 @@ export default function GenericCoinScreen({ navigation, route }) {
 
       <View style={styles.mainContentet}>
         <View style={styles.coinNameContent}>
-          <View
-            style={styles.nameContent}
-          >
+          <View style={styles.nameContent}>
             <Text style={styles.coinName}>{route.params.name}</Text>
           </View>
           <Text style={styles.initialsCoin}>({route.params.domainName})</Text>
         </View>
 
         <View style={styles.mainInfos}>
-          <View style={styles.cardInfo}>
-            <Text style={styles.typeInfo}>Ultima{'\n'}Cotação</Text>
+          <TouchableOpacity
+            style={styles.cardInfo}
+            onPress={() => setModalVisible(true)}
+          >
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                Alert.alert(
+                  "A Ultima cotação, representa a ultima cotação da moeda no dia até o momento da cotação."
+                );
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <Text style={styles.modalText}>
+                    A Ultima cotação, representa a ultima cotação da moeda no
+                    dia até o momento da cotação.
+                  </Text>
+                  <TouchableOpacity
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() => setModalVisible(!modalVisible)}
+                  >
+                    <Text style={styles.textStyle}>OK</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </Modal>
+            <Text style={styles.typeInfo}>Ultima{"\n"}Cotação</Text>
             <Text style={styles.valueCoin}>
               R$ {parseFloat(lastCoinSale).toFixed(2).replace(".", ",")}
             </Text>
-          </View>
-          <View style={styles.cardInfo}>
-            <Text style={styles.typeInfo}>Maior{'\n'}Venda/Hoje</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            TouchableOpacity
+            style={styles.cardInfo}
+            onPress={() => setLastSaleModalVisible(true)}
+          >
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={lastSaleModalVisible}
+              onRequestClose={() => {
+                Alert.alert(
+                  "A maior venda, representa a maior venda da moeda no dia até o momento da cotação."
+                );
+                setModalVisible(!lastSaleModalVisible);
+              }}
+            >
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <Text style={styles.modalText}>
+                    A maior venda, representa a maior venda da moeda no dia até
+                    o momento da cotação.
+                  </Text>
+                  <TouchableOpacity
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() =>
+                      setLastSaleModalVisible(!lastSaleModalVisible)
+                    }
+                  >
+                    <Text style={styles.textStyle}>OK</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </Modal>
+            <Text style={styles.typeInfo}>Maior{"\n"}Venda/Hoje</Text>
             <Text style={styles.valueCoin}>
               R$ {parseFloat(highCoinSale).toFixed(2).replace(".", ",")}
             </Text>
-          </View>
-          <View style={styles.cardInfo}>
-            <Text style={styles.typeInfo}>Valor{'\n'}Abertura</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.cardInfo}
+            onPress={() => setOpenValueModalVisible(true)}
+          >
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={openValueModalVisible}
+              onRequestClose={() => {
+                Alert.alert(
+                  "O valor abertura, representa o preço por unidade de abertura de negociação no dia até o momento da cotação."
+                );
+                setOpenValueModalVisible(!openValueModalVisible);
+              }}
+            >
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <Text style={styles.modalText}>
+                    O valor abertura, representa o preço por unidade de abertura
+                    de negociação no dia até o momento da cotação.
+                  </Text>
+                  <TouchableOpacity
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() =>
+                      setOpenValueModalVisible(!openValueModalVisible)
+                    }
+                  >
+                    <Text style={styles.textStyle}>OK</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </Modal>
+            <Text style={styles.typeInfo}>Valor{"\n"}Abertura</Text>
             <Text style={styles.valueCoin}>
               R$ {parseFloat(openValue).toFixed(2).replace(".", ",")}
             </Text>
-          </View>
-          <View style={styles.cardInfoLast}>
-            <Text style={styles.typeInfo}>Volume{'\n'}Hoje</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.cardInfo}
+            onPress={() => setVolModalVisible(true)}
+          >
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={volModalVisible}
+              onRequestClose={() => {
+                Alert.alert(
+                  "O volume, representa a quantidade negociada nas ultimas 24 horas até a cotação."
+                );
+                setVolModalVisible(!volModalVisible);
+              }}
+            >
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <Text style={styles.modalText}>
+                    O volume, representa a quantidade negociada nas ultimas 24
+                    horas até a cotação.
+                  </Text>
+                  <TouchableOpacity
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() => setVolModalVisible(!volModalVisible)}
+                  >
+                    <Text style={styles.textStyle}>OK</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </Modal>
+            <Text style={styles.typeInfo}>Volume{"\n"}Hoje</Text>
             <Text style={styles.valueCoin}>
               {parseFloat(last24Vol).toFixed(2).replace(".", ",")}
             </Text>
-          </View>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
--- src/components/GenericCoinScreen/styles.js
+++ src/components/GenericCoinScreen/styles.js
@@ -146,6 +146,54 @@ const styles = StyleSheet.create({
   mainInfos:{
     padding: 5,
     borderRadius: 10
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "#434343",
+    borderRadius: 10,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 999,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "transparent",
+    width: 70,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 70,
+  },
+  buttonClose: {
+    backgroundColor: "#B22222",
+    width: 100,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    fontWeight: "300",
+    color: "white"
   }
 });
 
