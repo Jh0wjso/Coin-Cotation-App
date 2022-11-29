4d0035861b0a34adf6a5c4eb50587b050fe8f43a
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-11-28 21:12:46 -0300

    Web Project Created

---
+++ Client/src/screens/Inicio/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function Inicio() {
+  return (
+    <section className="container flex items-center justify-center">
+      <p className="text-5xl mt-10">
+        Inicio
+      </p>
+    </section>
+  );
+}
