869f61ed2abb85a722363556fd19a03609d5e41f
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-12-11 15:42:44 -0300

    Download component update

--- Client/src/components/DowloadComponent/index.tsx
+++ Client/src/components/DowloadComponent/index.tsx
@@ -6,13 +6,13 @@ import { Link } from "react-router-dom";
 export default function DownloadComponent() {
   return (
     <div className="container p-11 text-white flex flex-col text-center">
-      <h1 className="text-center text-2xl md:text-5xl mb-5 font-bold uppercase italic">Faça o download, é gratis!</h1>
+      <h1 className="text-center text-2xl md:text-5xl mb-5 font-bold uppercase">Faça o download, é gratis!</h1>
       <div className="containerContent">
-        <div className="flex flex-col items-center bg-gray-600 h-auto p-5 justify-center rounded mb-10">
-          <h1 className="text-xl md:text-3xl md:mb-0 mb-5 text-red-700 md:uppercase font-bold">Faça o download do app</h1>
+        <div className="flex flex-col items-center bg-red-900 h-auto p-5 justify-center rounded-2xl mb-10">
+          <h1 className="text-xl md:text-3xl md:mb-0 mb-5 text-white md:uppercase font-medium">Faça o download do app</h1>
           <div className="mt-3">
-            <a href={""} target="_blank" >
-              <img src={GooglePlay} className="hover:opacity-75" width={300} />
+            <a href={"/"} target="_blank" >
+              <img src={GooglePlay} className="hover:opacity-70" width={300} />
             </a>
             <p className="text-center font-bold">Clique aqui para baixar</p>
           </div>
--- Client/src/screens/DownloadScreen/index.tsx
+++ Client/src/screens/DownloadScreen/index.tsx
@@ -5,10 +5,9 @@ import "./styles.css";
 export default function DownloadScreen() {
   return (
     <section className="downloadContent min-h-screen w-full p-11 flex flex-col justify-center items-center text-white">
-      <div className="bg-[#292828c0] container rounded md:rounded-2xl w-full">
+      <div className="bg-[#292828c9] container rounded md:rounded-2xl w-full">
         <DownloadComponent/>
       </div>
-      
     </section>
   );
 }
