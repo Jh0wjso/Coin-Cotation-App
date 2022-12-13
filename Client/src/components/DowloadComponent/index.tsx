fb676366c4dde0fa1ae5d9a190159d641c432d6f
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-12-12 22:52:56 -0300

    Updates on download screen

--- Client/src/components/DowloadComponent/index.tsx
+++ Client/src/components/DowloadComponent/index.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export default function DownloadComponent() {
   return (
     <div className="container p-11 text-white flex flex-col text-center">
-      <h1 className="text-center text-2xl md:text-5xl mb-5 font-bold uppercase">Faça o download, é gratis!</h1>
+      <h1 className="text-center text-2xl md:text-5xl mb-5 font-bold uppercase">Faça o download, é grátis!</h1>
       <div className="containerContent">
         <div className="flex flex-col items-center bg-red-900 h-auto p-5 justify-center rounded-2xl mb-10">
           <h1 className="text-xl md:text-3xl md:mb-0 mb-5 text-white md:uppercase font-medium">Faça o download do app</h1>
