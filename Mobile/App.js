9d595b3e3684a9e3d67f04498ee874cbfb42e01d
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-02-11 11:14:29 -0300

    Restructuring the project

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import Routes from './src/components/routes';
+import Routes from './src/screens/routes';
 
 export default function App() {
   return (
